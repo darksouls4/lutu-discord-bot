5d5e35c9949189a27c1860f5f05f873bafc71522
MrAugu <39545629+MrAugu@users.noreply.github.com>
2019-09-08 19:14:40 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,76 @@
+const config = {
+  "token":  "CLIENT_TOKEN",
+  "prefix": "?",
+  "admins": ["],
+  "dbUrl": "MONGODB_URL",
+  "patreons": [],
+  "supporters": [],
+  
+  "dashboard" : {
+    "oauthSecret": "CLIENT_SECRET",
+    "callbackURL": "http://localhost/callback", // add this to callback urls in your application's OAuth tab
+    "sessionSecret": "ohmulol12445$@%#%#fcdshfuiwedfkh",
+    "domain": "localhost",
+    "port": 80
+  },
+
+  permLevels: [
+    { level: 0,
+      name: "User",
+      check: () => true
+    },
+  
+    { level: 2,
+      name: "Moderator",
+      check: (message) => {
+        try {
+          if (message.member.hasPermission("MANAGE_MESSAGES") || message.member.hasPermission("MANAGE_GUILD") ||  message.member.roles.get(message.guild.settings.modRole) !== undefined) {
+            return true;
+          } else {
+            return false;
+          }
+        } catch (e) {
+          return false;
+        }
+      }
+    },
+  
+    { level: 3,
+      name: "Administrator",
+      check: (message) => {
+        try {
+          if (message.member.hasPermission("ADMINISTRATOR") ||  message.member.roles.get(message.guild.settings.adminRole) !== undefined) {
+            return true;
+          } else {
+            return false;
+          }
+        } catch (e) {
+          return false;
+        }
+      }
+    },
+  
+    { level: 4,
+      name: "Server Owner",
+      check: (message) => {
+        if (message.channel.type === "text" && message.guild.ownerID) {
+          if (message.guild.ownerID === message.author.id) return true;
+        } else {
+          return false;
+        }
+      }
+    },
+
+    { level: 9,
+      name: "Bot Admin",
+      check: (message) => config.admins.includes(message.author.id)
+    },
+  
+    { level: 10,
+      name: "Bot Owner",
+      check: (message) => message.client.appInfo.owner.id === message.author.id
+    }
+  ]
+};
+  
+module.exports = config;
